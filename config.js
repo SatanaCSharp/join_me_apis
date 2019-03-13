349d1e00a5c37ccf91f39a438a63474632487df9
Yurii <yurii.horchuk@gmail.com>
2019-03-13 17:16:54 +0200

    Initialize project

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    server: {
+        host: '127.0.0.1',
+        port: 4000
+    },
+
+    baseUrl: 'http://localhost:4000',
+
+    db: {
+        mongo: {
+            url: 'mongodb://localhost:27019/join_me'
+        },
+    },
+
+    auth: {
+        tokenLifetime: 7200000
+    },
+
+    headers: {
+        authToken: 'auth-token'
+    },
+
+    secret: 'join_me_secret_rfn11'
+};
