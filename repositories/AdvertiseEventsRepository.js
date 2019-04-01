88643d498be1b7197ab954fc59cead5045c22ab4
Yurii <yurii.horchuk@gmail.com>
2019-04-01 10:05:14 +0300

    Task-#60 (require manual testing)

--- repositories/AdvertiseEventsRepository.js
+++ repositories/AdvertiseEventsRepository.js
@@ -1,18 +1,28 @@
 const User = require('../models/User');
 const Event = require('../models/Event');
 
-async function show(userId) {
-    const user = await User.findById(userId, '_id').populate('categories');
-    const events = [];
-    await user.categories.forEach(function (category) {
-        category.tags.forEach(function (tag) {
-            events.push(Event.find({tags: [tag]}).exec());
-        });
-    });
-    console.log(events);
-    return await events;
+async function findEventsByTag(tag) {
+    return await Event.find({tags: [tag]})
+        .populate('address')
+        .populate('user', 'firstName lastName');
 }
 
+async function findAdvisedEvents(user) {
+    let events = [];
+    for (let category of   user.categories) {
+        for (let tag of category.tags) {
+            let currentEvent = await findEventsByTag(tag);
+            events.push(...currentEvent);
+        }
+    }
+    return events;
+}
+
+async function show(userId) {
+    const user = await User.findById(userId, '_id').populate('categories');
+    const events = await findAdvisedEvents(user);
+    return await events;
+}
 
 module.exports = {
     show
