f86bb0d3b0cacad23816563d9569ec7dd79fa0ee
Yurii <yurii.horchuk@gmail.com>
2019-05-03 10:03:19 +0300

    Task-#56

--- controllers/AuthFacebookController.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    authenticate: require('./actions/auth_facebook/authenticate')
-};
--- routes/authFacebook.js
+++ routes/authFacebook.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const AuthFacebookController = require('../controllers/AuthFacebookController');
+const AuthSocialController = require('../controllers/AuthSocialController');
 const AuthFacebookMiddleware = require('../middlewares/AuthFacebookMiddleware');
 router.get('/authorize', AuthFacebookMiddleware.authorize);
-router.get('/authenticate', AuthFacebookMiddleware.authenticate, AuthFacebookController.authenticate);
+router.get('/authenticate', AuthFacebookMiddleware.authenticate, AuthSocialController.authenticate);
 module.exports = router;
